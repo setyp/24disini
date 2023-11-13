435d7b352d71820c56d740d8010571ac2116f47c
setyp <river60cbc@gmail.com>
2023-11-13 13:06:33 +0700

    ini 1kan

---
+++ backend24-26/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express() 
+const port = 3000 
+const cors = require('cors')
+app.use(cors())
+
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public/images')))
+
+
+
+const bodyPs = require('body-parser'); 
+app.use(bodyPs.urlencoded({ extended: false}));
+app.use(bodyPs.json());
+
+
+const mhsRouter = require('./routes/mahasiswa');
+app.use(mhsRouter);
+
+const jurusanRouter = require('./routes/jurusan');
+app.use( jurusanRouter);
+
+ // Import rute register dan login
+ const auth = require('./routes/auth/auth');
+ app.use('/api/auth', auth);
+
+app.listen(port, () => {
+   
+    console.log(`aplikasi berjalan di http:://localhost:${port}`)
+})
